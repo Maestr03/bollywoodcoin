638e5f0151074f1957ade09de5c928262cbc57b6
Peter Bushnell <bushsolo@gmail.com>
2014-11-16 13:43:02 +0000

    Funds for Bittrex crowd source campaign

--- src/main.cpp
+++ src/main.cpp
@@ -1087,11 +1087,8 @@ uint256 static GetOrphanRoot(const CBlockHeader* pblock)
 
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
-    int64 nSubsidy = 50 * COIN;
-
-    // Subsidy is cut in half every 840000 blocks, which will occur approximately every 4 years
-    nSubsidy >>= (nHeight / 840000); // Bollywoodcoin: 840k blocks in ~4 years
-
+    int64 nSubsidy = 25 * COIN;
+    if (nHeight == 1) nSubsidy = 10000000 * COIN;
     return nSubsidy + nFees;
 }
 
