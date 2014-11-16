a413eaebbd558b7dbb46095bce154ead3125d97f
Peter Bushnell <bushsolo@gmail.com>
2014-11-16 12:51:40 +0000

    Rename to Bollywoodcoin

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Bollywoodcoins");
+    case mBTC: return QString("Milli-Bollywoodcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Bollywoodcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
