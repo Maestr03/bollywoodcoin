44c20ca80b2510b7cf5b1770a102247065561075
Peter Bushnell <bushsolo@gmail.com>
2014-11-16 12:54:44 +0000

    Rename to BDC

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -38,9 +38,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("BDC");
+    case mBTC: return QString("mBDC");
+    case uBTC: return QString::fromUtf8("μBDC");
     default: return QString("???");
     }
 }
