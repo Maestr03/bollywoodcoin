e8133dd01b27f92265523017bc415c8cd5496136
Peter Bushnell <bushsolo@gmail.com>
2014-11-16 13:34:28 +0000

    P2P and RPC ports

--- src/qt/locale/bitcoin_nl.ts
+++ src/qt/locale/bitcoin_nl.ts
@@ -2239,8 +2239,8 @@ Adres: %4
     </message>
     <message>
         <location line="-28"/>
-        <source>Listen for connections on &lt;port&gt; (default: 9333 or testnet: 19333)</source>
-        <translation>Luister voor verbindingen op &lt;poort&gt; (standaard: 9333 of testnet: 19333)</translation>
+        <source>Listen for connections on &lt;port&gt; (default: 9114 or testnet: 19114)</source>
+        <translation>Luister voor verbindingen op &lt;poort&gt; (standaard: 9114 of testnet: 19114)</translation>
     </message>
     <message>
         <location line="+5"/>
@@ -2274,8 +2274,8 @@ Adres: %4
     </message>
     <message>
         <location line="+27"/>
-        <source>Listen for JSON-RPC connections on &lt;port&gt; (default: 9332 or testnet: 19332)</source>
-        <translation>Wacht op JSON-RPC-connecties op poort &lt;port&gt; (standaard: 9332 of testnet: 19332)</translation>
+        <source>Listen for JSON-RPC connections on &lt;port&gt; (default: 9113 or testnet: 19113)</source>
+        <translation>Wacht op JSON-RPC-connecties op poort &lt;port&gt; (standaard: 9113 of testnet: 19113)</translation>
     </message>
     <message>
         <location line="+37"/>
